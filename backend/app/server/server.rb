5b50434f4c0981020c845514ad7143cd42cd7115
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-03-09 02:28:31 +0900

    add server.rb

--- backend/app/server/server.rb
+++ backend/app/server/server.rb
@@ -2,30 +2,22 @@
 
 require "grpc"
 require "/.lib/router_services_pb.rb"
+require "./service/message_service.rb"
+require "./service/room_service.rb"
+require "./service/user_service.rb"
 
 
-class ServerImpl < Serviveragent::Serviveragent::Railschat::Com::Protobuf::Proto::RoomService::Service
-  # これはRoomServiceのControllerしか定義していない
-  # 実際は RoomService, UserService, MessageServieを作りその中で実装
-  # そのクラスをServerImplでインスタンス化するべき
-  def create_room(create_room_request, _call)
-    room_id = 1
-    CreateRoomResponse.new(room_id)
+class Server
+  def main
+    port = "0.0.0.0:50051"
+    server = GRPC::RpcServer.new
+    server.add_http2_port(port, :this_port_is_insecure)
+    GRPC.logger.info("... running insecurely on #{port}")
+    server.handle(MessageService.new())
+    server.handle(RoomService.new())
+    server.handle(UserService.new())
+    server.run_till_terminated
   end
 
-  def update_room(update_room_request, _call)
-    UpdateRoomResponse
-  end
-
-  def update_room_user(update_room_user_request, _call)
-    UpdateRoomUserResponse
-  end
-
-  def list_room(list_room_request, _call)
-    ListRoomResponse
-  end
-
-  def delete_room(delete_room_request, _call)
-    DeleteRoomResponse
-  end
+  main
 end
