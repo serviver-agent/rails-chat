74604c90540f2b1fb4de2192c5ce897536c0000f
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-03-13 01:31:45 +0900

    rubocop

--- backend/app/server/server.rb
+++ backend/app/server/server.rb
@@ -7,14 +7,14 @@ require "./app/service/user_service.rb"
 
 class Server
   # def main
-    port = "0.0.0.0:3000"
-    server = GRPC::RpcServer.new
-    server.add_http2_port(port, :this_port_is_insecure)
-    GRPC.logger.info("... running insecurely on #{port}")
-    server.handle(MessageService.new())
-    server.handle(RoomService.new())
-    server.handle(UserService.new())
-    server.run_till_terminated
+  port = "0.0.0.0:3000"
+  server = GRPC::RpcServer.new
+  server.add_http2_port(port, :this_port_is_insecure)
+  GRPC.logger.info("... running insecurely on #{port}")
+  server.handle(MessageService.new())
+  server.handle(RoomService.new())
+  server.handle(UserService.new())
+  server.run_till_terminated
   # end
 
   # main
